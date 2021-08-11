58e14e3ff650f2a370e0fc722b1cca82429eb894
gofreeka <gabormartha.mail@gmail.com>
2021-08-11 15:34:01 +0200

    conduit test test_cookie()

--- tests/test_second.py
+++ tests/test_second.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 # import time
-from cookie import test_cookie
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')
@@ -9,4 +8,31 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 URL = 'http://localhost:1667/'
 driver.get(URL)
 
-test_cookie()
+# Adatvedelmi nyilatkozat hasznalata
+
+
+def test_cookie():
+
+    def check_cookies():
+        cookies = driver.get_cookies()
+        for coo in cookies:
+            print(coo)
+
+    check_cookies()
+
+    cookie_bar = driver.find_element_by_id("cookie-policy-panel")
+    assert cookie_bar.is_displayed()
+    accept = driver.find_element_by_xpath("//button[normalize-space()='I accept!']")
+
+    accept.click()
+    driver.refresh()
+    check_cookies()
+    driver.delete_all_cookies()
+
+    driver.refresh()
+    check_cookies()
+
+    decline = driver.find_element_by_xpath("//button[normalize-space()='I decline!']")
+    decline.click()
+    check_cookies()
+
