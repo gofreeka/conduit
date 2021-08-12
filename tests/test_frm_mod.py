af594b992c03552bbdce76eb89b7ea7ae3d7b5a6
gofreeka <gabormartha.mail@gmail.com>
2021-08-12 16:07:23 +0200

    module cookie_mod

--- tests/test_frm_mod.py
+++ tests/test_frm_mod.py
@@ -1,12 +1,3 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-# import time
 from cookie import test_cookie
-
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
-URL = 'http://localhost:1667/'
-driver.get(URL)
 
 test_cookie()
