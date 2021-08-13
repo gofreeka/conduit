b3402473cdae6974ca599d1d841359e7a84c1055
gofreeka <gabormartha.mail@gmail.com>
2021-08-13 17:24:37 +0200

    new blogpost

--- tests/test_003.py
+++ tests/test_003.py
@@ -9,40 +9,51 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 URL = 'http://localhost:1667/'
 driver.get(URL)
 
-test_user_1 = ["testuser1", "testuser1@example.com", "Abcd123$"]
-test_user_2 = ["testuser2", "testuser2@example.com", "Abcd123$"]
-test_user_3 = ["testuser3", "testuser3@example.com", "Abcd123$"]
-test_user_4 = ["testuser4", "testuser4@example.com", "Abcd123$"]
-test_user_5 = ["testuser5", "testuser5@example.com", "Abcd123$"]
-
+# test_user_1 = ["testuser1", "testuser1@example.com", "Abcd123$"]
+# test_user_2 = ["testuser2", "testuser2@example.com", "Abcd123$"]
+# test_user_3 = ["testuser3", "testuser3@example.com", "Abcd123$"]
+# test_user_4 = ["testuser4", "testuser4@example.com", "Abcd123$"]
+# test_user_5 = ["testuser5", "testuser5@example.com", "Abcd123$"]
 
 # Bejelentkezes
 
 
-#
-# def test_001_sign_in():
-#
-#     time.sleep(2)
-#
-#     sign_in = driver.find_element_by_xpath("//a[normalize-space()='Sign in']")
-#     sign_in.click()
-#
-#     email = driver.find_element_by_xpath("//input[@placeholder='Email']")
-#     password = driver.find_element_by_xpath("//input[@placeholder='Password']")
-#
-#     email.send_keys(test_user_1[1])
-#     password.send_keys(test_user_1[2])
-#
-#     bt_sign_in = driver.find_element_by_xpath("//button[normalize-space()='Sign in']")
-#     bt_sign_in.click()
-#     time.sleep(3)
-
-
-def test_002_user_check():
+def test_001_login():
     test_001_sign_in()
 
-    assert test_user_1[0] == driver.find_element_by_xpath("//div[@id='app']/nav/div/ul/li[4]/a").text
 
+# NEW BLOG POST - CON-TC004
+
+
+def test_002_new_blog_post():
+
+    blog_post_data = {
+        "data_article_title": "My 1st post",
+        "data_article_about": "About the beginning",
+        "data_article_markdown": "There was nothing.",
+        "data_tag": "1st"
+    }
+
+    new_article = driver.find_element_by_xpath("//a[@href='#/editor']")
+    time.sleep(2)
+    new_article.click()
+    time.sleep(2)
+
+    article_title = driver.find_element_by_xpath("//input[@placeholder='Article Title']")
+    article_title.send_keys(blog_post_data.get("data_article_title"))
+
+    article_about = driver.find_element_by_xpath("//input[contains(@placeholder, 'this article about?')]")
+    article_about.send_keys(blog_post_data.get("data_article_about"))
+
+    article_markdown = driver.find_element_by_xpath("//textarea[contains(@placeholder, 'markdown')]")
+    article_markdown.send_keys(blog_post_data.get("data_article_markdown"))
+
+    tag = driver.find_element_by_xpath("//input[@placeholder='Enter tags']")
+    tag.send_keys(blog_post_data.get("data_tag"))
+
+    bt_publish = driver.find_element_by_xpath("//button[normalize-space()='Publish Article']")
+    bt_publish.click()
+    time.sleep(4)
 
 time.sleep(1)
 
