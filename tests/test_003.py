c476154fce08e87a21fe91b9f0d640b31cf5b187
gofreeka <gabormartha.mail@gmail.com>
2021-08-13 22:27:21 +0200

    sign in only

--- tests/test_003.py
+++ tests/test_003.py
@@ -21,52 +21,52 @@ driver.get(URL)
 
 
 # def test_001_login():
-test_sign_in()
 
 
 # NEW BLOG POST - CON-TC004
 
 
 def test_002_new_blog_post():
-    blog_post_data = {
-        "data_article_title": "My 1st post",
-        "data_article_about": "About the beginning",
-        "data_article_markdown": "There was nothing.",
-        "data_tag": "1st"
-    }
-
-    new_article = driver.find_element_by_xpath("//a[@href='#/editor']")
-    time.sleep(2)
-    new_article.click()
-    time.sleep(2)
-
-    article_title = driver.find_element_by_xpath("//input[@placeholder='Article Title']")
-    article_title.send_keys(blog_post_data.get("data_article_title"))
-
-    article_about = driver.find_element_by_xpath("//input[contains(@placeholder, 'this article about?')]")
-    article_about.send_keys(blog_post_data.get("data_article_about"))
-
-    article_markdown = driver.find_element_by_xpath("//textarea[contains(@placeholder, 'markdown')]")
-    article_markdown.send_keys(blog_post_data.get("data_article_markdown"))
-
-    tag = driver.find_element_by_xpath("//input[@placeholder='Enter tags']")
-    tag.send_keys(blog_post_data.get("data_tag"))
-
-    bt_publish = driver.find_element_by_xpath("//button[normalize-space()='Publish Article']")
-    bt_publish.click()
-    time.sleep(4)
-
-
-# LOGOUT
-
-
-def test_003_log_out():
-    username = driver.find_element_by_xpath("//div/nav/div/ul/li[4]/a")
-    assert "#/@testuser1/" == username.get_property("href")
-
-    log_out_bt = driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[5]/a')
-    log_out_bt.click()
-
-    # if test_user_1[0] in driver.current_url:
-    #     log_out = driver.find_element_by_xpath("//div[@id='app']/nav/div/ul/li[5]/a").click()
+    test_sign_in()
+#     blog_post_data = {
+#         "data_article_title": "My 1st post",
+#         "data_article_about": "About the beginning",
+#         "data_article_markdown": "There was nothing.",
+#         "data_tag": "1st"
+#     }
+#
+#     new_article = driver.find_element_by_xpath("//a[@href='#/editor']")
+#     time.sleep(2)
+#     new_article.click()
+#     time.sleep(2)
+#
+#     article_title = driver.find_element_by_xpath("//input[@placeholder='Article Title']")
+#     article_title.send_keys(blog_post_data.get("data_article_title"))
+#
+#     article_about = driver.find_element_by_xpath("//input[contains(@placeholder, 'this article about?')]")
+#     article_about.send_keys(blog_post_data.get("data_article_about"))
+#
+#     article_markdown = driver.find_element_by_xpath("//textarea[contains(@placeholder, 'markdown')]")
+#     article_markdown.send_keys(blog_post_data.get("data_article_markdown"))
+#
+#     tag = driver.find_element_by_xpath("//input[@placeholder='Enter tags']")
+#     tag.send_keys(blog_post_data.get("data_tag"))
+#
+#     bt_publish = driver.find_element_by_xpath("//button[normalize-space()='Publish Article']")
+#     bt_publish.click()
+#     time.sleep(4)
+#
+#
+# # LOGOUT
+#
+#
+# def test_003_log_out():
+#     username = driver.find_element_by_xpath("//div/nav/div/ul/li[4]/a")
+#     assert "#/@testuser1/" == username.get_property("href")
+#
+#     log_out_bt = driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[5]/a')
+#     log_out_bt.click()
+#
+#     # if test_user_1[0] in driver.current_url:
+#     #     log_out = driver.find_element_by_xpath("//div[@id='app']/nav/div/ul/li[5]/a").click()
     #     time.sleep(2)
