388592cac67d74aa6d8a972fdf0df4cbe3c4e542
gofreeka <gabormartha.mail@gmail.com>
2021-08-18 16:44:28 +0200

    log_out_test.py ok

--- tests/log_out_test.py
+++ tests/log_out_test.py
@@ -8,8 +8,6 @@ driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 URL = 'http://localhost:1667/'
 driver.get(URL)
 
-
-# Bejelentkezes
 test_user_1 = ["testuser1", "testuser1@example.com", "Abcd123$"]
 test_user_2 = ["testuser2", "testuser2@example.com", "Abcd123$"]
 test_user_3 = ["testuser3", "testuser3@example.com", "Abcd123$"]
@@ -18,11 +16,8 @@ test_user_5 = ["testuser5", "testuser5@example.com", "Abcd123$"]
 
 
 def test_001_sign_in():
-
-    time.sleep(5)
-
+    time.sleep(2)
     sign_in = driver.find_element_by_xpath("//a[@href='#/login']")
-    #sign_in = driver.find_element_by_xpath("//a[normalize-space()='Sign in']")
     sign_in.click()
 
     email = driver.find_element_by_xpath("//input[@placeholder='Email']")
@@ -40,13 +35,9 @@ def test_002_user_check():
     assert test_user_1[0] == driver.find_element_by_xpath("//div[@id='app']/nav/div/ul/li[4]/a").text
 
 
-# LOGOUT
-
-
 def test_003_log_out():
-    # username = driver.find_element_by_xpath("//div/nav/div/ul/li[4]/a")
-    # assert "#/@testuser1/" == username.get_property("href")
-
     log_out_bt = driver.find_element_by_xpath('//*[@id="app"]/nav/div/ul/li[5]/a')
     log_out_bt.click()
+    time.sleep(1)
+    assert driver.find_element_by_xpath("//a[@href='#/login']").is_displayed()
 
